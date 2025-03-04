61a863a9385d07109dc68beb2cf31cc1f448c381
saksh-sys <66247423+saksh-sys@users.noreply.github.com>
2025-03-04 19:37:17 +0530

    Update text_to_sign.py

--- text_to_sign.py
+++ text_to_sign.py
@@ -8,9 +8,11 @@ def text_to_sign(text):
     """Converts text to corresponding sign images."""
     images = []
     for char in text.upper():
-        img_path = os.path.join(DATASET_PATH, f"{char}.jpg")
+        folder_path = os.path.join(DATASET_PATH, char)  # Navigate into subfolder
+        img_path = os.path.join(folder_path, "1.jpg")  # Fetch "1.jpg" inside folder
+
         if os.path.exists(img_path):
             img = Image.open(img_path)
             images.append(img)
-    
+
     return images
